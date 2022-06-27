950db2d766f0314446fd7f562f5fb5a775032402
unknown <chepurnojv@gmail.com>
2022-06-27 16:22:53 +0300

    coplete CPU monitor app

---
+++ app/js/monitor.js
@@ -0,0 +1,99 @@
+const osu = require("node-os-utils");
+const { ipcRenderer } = require("electron");
+const path = require("path");
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+
+let cpuOverload;
+let alertFrequency;
+
+//get settings & values
+ipcRenderer.on("settings:get", (e, settings) => {
+  cpuOverload = +settings.cpuOverload;
+  alertFrequency = +settings.alertFrequency;
+});
+
+//run every 2 seconds
+setInterval(() => {
+  //cpu usage
+  cpu.usage().then((info) => {
+    document.getElementById("cpu-usage").innerText = info + "%";
+
+    document.getElementById("cpu-progress").style.width = info + "%";
+
+    //make progress be red on overload
+    if (info >= cpuOverload) {
+      document.getElementById("cpu-progress").style.background = "red";
+    } else {
+      document.getElementById("cpu-progress").style.background = "#30c88b";
+    }
+
+    //check overload
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: "CPU Overload",
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, "img", "icon.png"),
+      });
+
+      localStorage.setItem("lastNotify", +new Date());
+    }
+  });
+
+  //cpu free
+  cpu.free().then((info) => {
+    document.getElementById("cpu-free").innerText = info + "%";
+  });
+
+  document.getElementById("sys-uptime").innerText = secondsToDhms(os.uptime());
+}, 100);
+
+//set model
+document.getElementById("cpu-model").innerText = cpu.model();
+
+//computer name
+document.getElementById("comp-name").innerText = os.hostname();
+
+//operating system
+document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
+
+//total mem
+mem.info().then((info) => {
+  document.getElementById("mem-total").innerText = info.totalMemMb;
+});
+
+//shop uptime
+function secondsToDhms(seconds) {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
+
+//send notification
+function notifyUser(options) {
+  new Notification(options.title, options);
+}
+
+//check notification time
+function runNotify(frequency) {
+  if (localStorage.getItem("lastNotify") === null) {
+    //store timestamp
+    localStorage.setItem("lastNotify", +new Date());
+    return true;
+  }
+
+  const notifyTime = new Date(parseInt(localStorage.getItem("lastNotify")));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60));
+
+  if (minutesPassed > frequency) {
+    return true;
+  } else {
+    return false;
+  }
+}
